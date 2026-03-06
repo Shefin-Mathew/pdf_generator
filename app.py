09bdd8d48252793a8adac38075e3dea7db39b4f6
Shefin-Mathew <shefinn96@outlook.com>
2026-03-07 02:02:54 +0530

    pdf-1

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
 from reportlab.platypus import SimpleDocTemplate,Paragraph
 from reportlab.lib.styles import getSampleStyleSheet
+import pandas as pd
 
-
-
-def header_footer(canvas, doc):
+def header_footer(canvas,doc):
     canvas.saveState()
-
-    canvas.setFont("Times-Roman", 14)
-    canvas.drawString(50, 800, "My Notes")
-
-    canvas.setFont("Times-Roman", 12)
-    page_number = canvas.getPageNumber()
-    canvas.drawString(500, 20, f"Page {page_number}")
-
+    
+    canvas.setFont("Times-Roman","14")
+    canvas.drawString(50,100,"My Notes")
+    
+    canvas.setFont("Times-Roman","10")
+    page_number=canvas.getPageNumber()
+    canvas.drawString(500,20,f"Page {page_number}")
     canvas.restoreState()
     
-text_list = []
+text_list =[]
 
 while True:
-    line = input("Enter a text: ")
+    line = input("enter a text: ")
     if line.lower() == "end":
         break
     else:
@@ -31,5 +29,5 @@ story = []
 for text in text_list:
     story.append(Paragraph(text,styles["Normal"]))
 
-pdf = SimpleDocTemplate("output.pdf")
+pdf=SimpleDocTemplate("output.pdf")
 pdf.build(story,onFirstPage=header_footer,onLaterPages=header_footer)
